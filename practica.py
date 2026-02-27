121581e3404679d2f956b0dc1083ce5ed6fe00ec
rekiel7 <danielchacon151@gmail.com>
2026-02-26 20:47:36 -0500

    practica de calculadora

---
+++ practica.py
@@ -0,0 +1,16 @@
+print("porfavor ingrese un numero")
+num1 = int(input("ingrese el primer valor "))
+num2 = int(input("ingrese el segundo valor "))
+suma = num1 + num2
+resta = num1 - num2
+print("la suma es: ", suma)
+print("la resta es: ", resta)
+
+multiplicacion = num1 * num2
+print("la multiplicacion es: ", multiplicacion)
+
+division = num1 / num2
+print("el valor dividido es: ", division)
+
+
+
